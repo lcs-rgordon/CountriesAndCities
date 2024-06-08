b01d39de08901d4d6974aa22895aabe418c26142
Russell Gordon <rgordon@lcs.on.ca>
2024-06-08 13:21:42 -0400

    Added a model to represent a country.

---
+++ CountriesAndCities/Model/Country.swift
@@ -0,0 +1,16 @@
+//
+//  Country.swift
+//  CountriesAndCities
+//
+//  Created by Russell Gordon on 2024-06-08.
+//
+
+import Foundation
+
+struct Country: Identifiable, Codable {
+    
+    // MARK: Stored properties
+    var id: Int?
+    let name: String
+    
+}
