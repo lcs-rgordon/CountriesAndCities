a03765150a82e3fd9efb1a214dea3acc6007ae0f
Russell Gordon <rgordon@lcs.on.ca>
2024-06-08 13:03:27 -0400

    Initial Commit

---
+++ CountriesAndCities/CountriesAndCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAndCitiesApp.swift
+//  CountriesAndCities
+//
+//  Created by Russell Gordon on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAndCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
